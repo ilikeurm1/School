b459e2dada1076c1c21ed679399ff67c096ad553
@certiman <maarten.duhoux@gmail.com>
2023-01-26 22:28:41 +0100

    Update 5. Blood type.py
    Shorter is better as it is very readable too

--- Dodona Oefeningen/6. Extra oef/5. Blood type.py
+++ Dodona Oefeningen/6. Extra oef/5. Blood type.py
@@ -1,14 +1,10 @@
 Letter1 = str(input('Letter 1: '))
 Letter2 = str(input('Letter 2: '))
 
-if Letter1 == 'A' and Letter2 == 'B':
+if ( Letter1 == 'A' and Letter2 == 'B' ) or (Letter1 == 'B' and Letter2 == 'A'):
     Result = 'AB'
-elif Letter1 == 'B' and Letter2 == 'A':
-    Result = 'AB'
-elif Letter1 == 'O' and Letter2 != Letter1:
-    Result = Letter2
-elif Letter2 == 'O' and Letter1 != Letter2:
-    Result = Letter1
+elif (Letter1 == 'O' or Letter2 == 'O') and Letter1 != Letter2:
+    Result = Letter2 if Letter2!='O' else Letter1
 elif Letter1 == Letter2:
     Result = Letter1
 
