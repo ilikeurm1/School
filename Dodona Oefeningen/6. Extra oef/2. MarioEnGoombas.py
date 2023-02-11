0e4ea494e8a94bcab7b33fba31a7876485dce8a4
@certiman <maarten.duhoux@gmail.com>
2023-02-11 18:09:04 +0100

    Correction after some test runs

--- Dodona Oefeningen/6. Extra oef/2. MarioEnGoombas.py
+++ Dodona Oefeningen/6. Extra oef/2. MarioEnGoombas.py
@@ -11,33 +11,31 @@ de richting waarin Goomba B loopt: ook L of R
 ## CONSTANTS
 v = 1 # meter / minute
 
-## inputs
+## inputs and initial speed vectors
 y = int(input("Afstand y tussen bloempot en afgrond (m): "))
 A = int(input("Afstand A tussen Goomba A en afgrond (m): ")) # initial position of Goomba A
-B = int(input("Afstand B tussen Goomba B en afgrond (m): ")) # idem for B
-
-## initial speed vectors
 VA = v if input("Start Goomba A naar Links of Rechts (L/R) ?") == "L" else -v
+B = int(input("Afstand B tussen Goomba B en afgrond (m): ")) # idem for B
 VB = v if input("Start Goomba B naar Links of Rechts (L/R) ?") == "L" else -v
 
-T = 0 # expired minutes
+T = -1 # expired minutes, seems tests assume first move DONE at T=0
 
 ## Let the goomba's run as long as they are not in the AG
-while (A >= X or B= > x):
-  A=A+VA
-  B=B+VB
-  # check for collision with BP
-  if A = y: 
-    VA=-VA
-  if B = y:
-    VB=-VB
-  if A = B:
-    VB = -VB
-    VA = -VA
-  T = T + 1
-  if (T = 10 * y):
-    # Something is wrong here
-    break
+while (A >= 0 or B >= 0):
+    A = A+VA
+    B = B+VB
+    # check for collision with BP
+    if A == y: 
+        VA = -VA
+    if B == y:
+        VB = -VB
+    if A == B:
+        VB = -VB
+        VA = -VA
+    T = T + 1
+    if (T = 10 * y):
+      # Something is wrong here
+      break
  
 print(f"Het duurt {T} minuten tot beide Goomba's weg zijn")
   
