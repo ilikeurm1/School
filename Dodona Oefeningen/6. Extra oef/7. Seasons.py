9afff3ac8da9863989ec06f7c3866b9b844555cb
@certiman <maarten.duhoux@gmail.com>
2023-02-11 14:46:27 +0100

    Smater use of datetime and removal of unnecessary code

--- Dodona Oefeningen/6. Extra oef/7. Seasons.py
+++ Dodona Oefeningen/6. Extra oef/7. Seasons.py
@@ -2,46 +2,44 @@
 
 # ---> Imports <---
 
-import math # I always just import this cuz I might use it ... maybe ... sometimes
+from datetime import datetime
 
 # ---> Variables <---
 
 Months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-Days = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
-Seasons = ['spring', 'summer', 'autumn', 'winter'] # also pretty useless xD
-Worked = None
-# ---> Inputs <---
 
+"""
+lente	21 maart	    20 juni
+zomer	21 juni	        22 september
+herfst	23 september	20 december
+winter	21 december	    20 maart
+"""
+leapYear = 2020 # 29 Feb is tested, so must be leap year
+startWinter = datetime(leapYear, 12, 21)
+startFall = datetime(leapYear, 9, 23)
+startSummer = datetime(leapYear, 6, 21)
+startSpring = datetime(leapYear, 3, 21)
+
+# ---> Inputs <---
 Day = int(input('Which day is it: '))
 Month = input('Which month is it: ')
 indexM = Months.index(Month)
-# indexD = Days.index(Day) # prob not gonna be used.
 
-# ---> Calculations <---
-
-if Day in Days and Month in Months:
+try: 
+    # ---> Calculations <---
+    myDate = datetime(leapYear, indexM + 1, Day)
     Worked = True
-    if (Day >= 21 and indexM == 11) or (indexM < 2):
-        print(f'It is winter on {Month} {Day}.') # winter
-    elif indexM == 2 and Day <= 20:
-        print(f'It is winter on {Month} {Day}.') # winter
-    
-
-    elif (Day >= 21 and indexM == 2) or (indexM < 5):
-        print(f'It is spring on {Month} {Day}.') # spring
-    elif indexM == 5 and Day <= 20:
-        print(f'It is spring on {Month} {Day}.') # spring
-
-
-    elif (Day >= 21 and indexM == 5) or (indexM < 8):
-        print(f'It is summer on {Month} {Day}.') # summer
-    elif indexM == 8 and Day <= 22:
-        print(f'It is summer on {Month} {Day}.') # summer
-            
-
+    if (myDate >= startWinter or myDate < startSpring):
+        Season='winter' # winter
+    elif (myDate >= startSpring and myDate < startSummer):
+        Season='spring' # spring
+    elif (myDate >= startSummer and myDate < startFall):
+        Season='summer' # summer
     else:
-        print(f'It is autumn on {Month} {Day}.') # autumn
+        Season = 'autumn' # autumn
+        
+    print(f'It is {Season} on {Month} {Day}.')
 
-else:
+except ValueError:
     Worked = False
     print(Worked)
